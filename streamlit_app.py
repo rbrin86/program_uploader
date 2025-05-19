23840d53c2a7d36e26f609e2ca8a4518a966b1e3
Ryan Brin <59397204+rbrin86@users.noreply.github.com>
2025-05-18 20:47:30 -0600

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 if "page" not in st.session_state:
     st.session_state.page = "we_earn"
-if "do_rerun" not in st.session_state:
-    st.session_state.do_rerun = False
+if "rerun_requested" not in st.session_state:
+    st.session_state.rerun_requested = False
 
 def navigate_to(page):
     st.session_state.page = page
-    st.session_state.do_rerun = True
+    st.session_state.rerun_requested = True
 
-# Render pages
+# Render the appropriate page
 if st.session_state.page == "we_earn":
     from we_earn import render_we_earn
     render_we_earn(navigate_to)
@@ -17,7 +17,7 @@ elif st.session_state.page == "program_upload":
     from program_upload import render_program_upload
     render_program_upload(navigate_to)
 
-# Call rerun only if flagged, and then clear the flag
-if st.session_state.do_rerun:
-    st.session_state.do_rerun = False
+# Now conditionally rerun, then reset flag
+if st.session_state.rerun_requested:
+    st.session_state.rerun_requested = False
     st.experimental_rerun()
